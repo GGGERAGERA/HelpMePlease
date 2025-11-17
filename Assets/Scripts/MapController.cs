95053b74975d7289445dfbe4754524ef0db576dd
DmitriyDantes1995 <grigorevdm1995@gmail.com>
2025-11-17 18:45:06 +0300

    GENERATION ANIHHILATION BLEAT
    Скрипт Рандомных генераций тайлов
    Скрипт Выбора рандомного куска для генерации
    Скрипт MapControl для контроля всей карты
    Качающая музыка для души

---
+++ Assets/Scripts/MapController.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+    public List<GameObject> terrainChunk;
+    public GameObject player;
+    public float checkerRadius;
+    Vector3 noTerrarinPosition;
+    public LayerMask terrarinMask;
+    public GameObject currentChunk;
+    PlayerMovement pm;
+
+  
+    void Start()
+    {
+        pm = Object.FindAnyObjectByType<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        ChunkChecker();
+    }
+
+    void ChunkChecker()
+    {
+        if(!currentChunk)
+        {
+            return;
+        }
+        if(pm.moveDir.x > 0 && pm.moveDir.y == 0) // right
+        {
+            if(!Physics2D.OverlapCircle(currentChunk.transform.Find("Right").position, checkerRadius, terrarinMask))
+            {
+                noTerrarinPosition = currentChunk.transform.Find("Right").position;
+                SpawnChunk();
+            }
+        }
+        else if (pm.moveDir.x < 0 && pm.moveDir.y == 0) // left 
+        {
+            if (!Physics2D.OverlapCircle(currentChunk.transform.Find("Left").position, checkerRadius, terrarinMask))
+            {
+                noTerrarinPosition = currentChunk.transform.Find("Left").position;
+                SpawnChunk();
+            }
+        }
+        else if (pm.moveDir.x == 0 && pm.moveDir.y > 0) // up 
+        {
+            if (!Physics2D.OverlapCircle(currentChunk.transform.Find("Up").position, checkerRadius, terrarinMask))
+            {
+                noTerrarinPosition = currentChunk.transform.Find("Up").position;
+                SpawnChunk();
+            }
+        }
+        else if (pm.moveDir.x == 0 && pm.moveDir.y < 0) // down 
+        {
+            if (!Physics2D.OverlapCircle(currentChunk.transform.Find("Down").position, checkerRadius, terrarinMask))
+            {
+                noTerrarinPosition = currentChunk.transform.Find("Down").position;
+                SpawnChunk();
+            }
+        }
+        else if (pm.moveDir.x > 0 && pm.moveDir.y > 0) // right up 
+        {
+            if (!Physics2D.OverlapCircle(currentChunk.transform.Find("Right Up").position, checkerRadius, terrarinMask))
+            {
+                noTerrarinPosition = currentChunk.transform.Find("Right Up").position;
+                SpawnChunk();
+            }
+        }
+        else if (pm.moveDir.x > 0 && pm.moveDir.y < 0) // right down
+        {
+            if (!Physics2D.OverlapCircle(currentChunk.transform.Find("Right Down").position, checkerRadius, terrarinMask))
+            {
+                noTerrarinPosition = currentChunk.transform.Find("Right Down").position;
+                SpawnChunk();
+            }
+        }
+        else if (pm.moveDir.x < 0 && pm.moveDir.y > 0) // left up 
+        {
+            if (!Physics2D.OverlapCircle(currentChunk.transform.Find("Left Up").position, checkerRadius, terrarinMask))
+            {
+                noTerrarinPosition = currentChunk.transform.Find("Left Up").position;
+                SpawnChunk();
+            }
+        }
+        else if (pm.moveDir.x < 0 && pm.moveDir.y < 0) // left down 
+        {
+            if (!Physics2D.OverlapCircle(currentChunk.transform.Find("Left Down").position, checkerRadius, terrarinMask))
+            {
+                noTerrarinPosition = currentChunk.transform.Find("Left Down").position;
+                SpawnChunk();
+            }
+        }
+
+    }
+
+    void SpawnChunk()
+    {
+        int rand = Random.Range(0, terrainChunk.Count);
+        Instantiate(terrainChunk[rand], noTerrarinPosition, Quaternion.identity);
+    }
+}
