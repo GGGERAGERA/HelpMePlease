65cf94c204721cdefbc20bf3ae1994539ff899e1
DmitriyDantes1995 <grigorevdm1995@gmail.com>
2025-11-19 17:23:11 +0300

    Weapon and Enemy
    Первое оружие (ближнее и дальнее)
    Первый враг
    Оптимизация генерации

---
+++ Assets/Scripts/Weapons/WeaponController/GarlicController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GarlicController : WeaponController
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+
+    protected override void Attack()
+    {
+        base.Attack();
+        GameObject spawnedGarlic = Instantiate(prefab);
+        spawnedGarlic.transform.position = transform.position; // Assing the position to be the same as this object which is parented to the player
+        spawnedGarlic.transform.parent = transform;
+    }
+}
