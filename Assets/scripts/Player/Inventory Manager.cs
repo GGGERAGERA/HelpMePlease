e053c30d4241c9e1775e55ed94f83b2d01a6cbef
DANTES <grigorevdm1995@gmail.com>
2025-11-27 23:48:41 +0300

    Lesson12_completed
    Работа с инвентарем и пользовательским интерфейсом во время игры

--- Assets/scripts/Player/Inventory Manager.cs
+++ Assets/scripts/Player/Inventory Manager.cs
@@ -1,32 +1,67 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
 
     public List<WeaponController> weaponSlots = new List<WeaponController>(6);
     public int[] weaponLevels = new int[6];
+    public List<Image> weaponUISlots = new List<Image>(6);
     public List<PassiveItem> passiveItemSlots = new List<PassiveItem>(6);
     public int[] passiveItemLevels = new int[6];
+    public List<Image> passiveItemUISlots = new List<Image>(6);
 
     public void AddWeapon(int slotIndex, WeaponController weapon)
     {
         weaponSlots[slotIndex] = weapon;
+        weaponLevels[slotIndex] = weapon.weaponData.Level;
+        weaponUISlots[slotIndex].enabled = true;
+        weaponUISlots[slotIndex].sprite = weapon.weaponData.Icon;
     }
 
     public void AddPassiveItem(int slotIndex, PassiveItem passiveItem)
     {
-        passiveItemSlots[slotIndex] = passiveItem; 
+        passiveItemSlots[slotIndex] = passiveItem;
+        passiveItemLevels[slotIndex] = passiveItem.passiveItemData.Level;
+        passiveItemUISlots[slotIndex].sprite = passiveItem.passiveItemData.Icon;
+        passiveItemUISlots[slotIndex].enabled = true;
     }
 
     public void LevelUpWeapon(int slotIndex)
     {
-
+        if ( weaponSlots.Count > slotIndex)
+        {
+            WeaponController weapon = weaponSlots[slotIndex];
+            if (!weapon.weaponData.NextLevelPrefab)
+            {
+                Debug.LogError("NO NEXT LEVEL FOR " + weapon.name);
+                return;
+            }
+            GameObject upgradedWeapon = Instantiate(weapon.weaponData.NextLevelPrefab, transform.position, Quaternion.identity);
+            upgradedWeapon.transform.SetParent(transform);
+            AddWeapon(slotIndex, upgradedWeapon.GetComponent<WeaponController>());
+            Destroy(weapon.gameObject);
+            weaponLevels[slotIndex] = upgradedWeapon.GetComponent<WeaponController>().weaponData.Level;
+        }
     }
 
     public void LevelUpPassiveItem(int slotIndex)
     {
-
+        if (passiveItemSlots.Count > slotIndex)
+        {
+            PassiveItem passiveItems = passiveItemSlots[slotIndex];
+            if (!passiveItems.passiveItemData.NextLevelPrefab)
+            {
+                Debug.LogError("NO NEXT LEVEL FOR " + passiveItems.name);
+                return;
+            }
+            GameObject upgradedPassiveItem = Instantiate(passiveItems.passiveItemData.NextLevelPrefab, transform.position, Quaternion.identity);
+            upgradedPassiveItem.transform.SetParent(transform);
+            AddPassiveItem(slotIndex, upgradedPassiveItem.GetComponent<PassiveItem>());
+            Destroy(passiveItems.gameObject);
+            passiveItemLevels[slotIndex] = upgradedPassiveItem.GetComponent<PassiveItem>().passiveItemData.Level;
+        }
     }
 
 }
