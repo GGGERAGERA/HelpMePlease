9740eb088bb0704097ae475967035587bc49384a
german <german@yohoho.games>
2025-11-30 01:35:39 +0300

    delete all previos content

--- Assets/scripts/Passive Items/SpinachPassiveItem.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class SpinachPassiveItem : PassiveItem
-{
-    protected override void ApplyModifier()
-    {
-        player.currentMight *= 1 + passiveItemData.Multipler / 100f;
-    }
-}
