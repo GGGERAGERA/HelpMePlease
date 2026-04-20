e80ff69b61d27816df241c54cf5866db4c5ffaaa
DANTES <grigorevdm1995@gmail.com>
2026-04-21 00:23:11 +0300

    DamageFlash

--- Assets/_Project/scripts/Enemy/EnemyHealth.cs
+++ Assets/_Project/scripts/Enemy/EnemyHealth.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
-using UnityEngine.Events; // обязательно добавить
+using UnityEngine.Events;
 
 public class EnemyHealth : MonoBehaviour
 {
     public float maxHealth = 30f;
     private float currentHealth;
+    private bool isDead = false;
 
-    public UnityEvent<float, float> OnHealthChanged; // (current, max)
+    public UnityEvent<float, float> OnHealthChanged;
     public UnityEvent onDeath;
+    public UnityEvent OnDamageTaken; // новое событие для эффекта урона
 
     void Start()
     {
@@ -17,15 +19,21 @@ public class EnemyHealth : MonoBehaviour
 
     public void TakeDamage(float damage)
     {
+        if (isDead) return;
         if (currentHealth <= 0) return;
+
         currentHealth -= damage;
         OnHealthChanged?.Invoke(currentHealth, maxHealth);
+        OnDamageTaken?.Invoke(); // вызываем эффект урона
+
         if (currentHealth <= 0)
             Die();
     }
 
     void Die()
     {
+        if (isDead) return;
+        isDead = true;
         onDeath?.Invoke();
     }
 }
