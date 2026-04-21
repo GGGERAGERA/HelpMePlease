4bfa5bda951dcd42333439d11b095e3d8d4d6148
DmitriyDantes1995 <grigorevdm1995@gmail.com>
2026-04-21 12:45:54 +0300

    GameOverManager

---
+++ Assets/_Project/scripts/UI/GameOverManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public static GameOverManager Instance;
+
+    public GameObject gameOverPanel; // сюда перетащите GameOverCanvas (который неактивен)
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+
+        // Убедимся, что панель скрыта при старте
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(false);
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0f;
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
