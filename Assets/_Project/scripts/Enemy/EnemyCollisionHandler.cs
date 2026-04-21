292a6df20ecab469ce973d1d3a1e4a05ef66f86e
DmitriyDantes1995 <grigorevdm1995@gmail.com>
2026-04-21 14:17:35 +0300

    пули без самонаведении, а по прямой траектории к врагу

--- Assets/_Project/scripts/Enemy/EnemyCollisionHandler.cs
+++ Assets/_Project/scripts/Enemy/EnemyCollisionHandler.cs
@@ -9,12 +9,6 @@ public class EnemyCollisionHandler : MonoBehaviour
         {
          collision.GetComponent<PlayerHealth>()?.TakeDamage(10); // наносим урон игроку, если он столкнулся с врагом
         }
-        else if (collision.CompareTag("Bullet"))
-        {
-            var bullet = collision.GetComponent<Bullet>();
-            GetComponent<EnemyHealth>()?.TakeDamage(bullet.damage); // наносим урон врагу, если он столкнулся с пулей
-            Destroy(collision.gameObject); // уничтожаем пулю после столкновения
-        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
