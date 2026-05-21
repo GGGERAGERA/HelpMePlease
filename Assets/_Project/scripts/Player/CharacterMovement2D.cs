d9df8f6744386b80c57d6f4aeded48c874ee3cb0
german <german@yohoho.games>
2026-05-22 00:23:01 +0300

    new player1

--- Assets/_Project/scripts/Player/CharacterMovement2D.cs
+++ Assets/_Project/scripts/Player/CharacterMovement2D.cs
@@ -43,6 +43,11 @@ public class CharacterMovement2D : MonoBehaviour
             Vector3 scale = visualRoot.localScale;
             scale.x = Mathf.Abs(scale.x) * -Mathf.Sign(moveInput.x);
             visualRoot.localScale = scale;
+
+            if (scale.x < 0)
+                visualRoot.GetComponent<SpriteRenderer>().flipX = true;
+            else
+                visualRoot.GetComponent<SpriteRenderer>().flipX = false;
         }
 
         if (animator != null)
